1ef12e856332498b98d0ffa4b10eedd3c0e30e4c
Bruno-dev1 <brunonasifalso@gmail.com>
2024-11-28 11:40:27 -0300

    ;

--- testes.js
+++ testes.js
@@ -1,10 +1,2 @@
-const nome = "adenaldo"
-let nome_alt =""
-for (letra of nome) {
-    if (letra == "a") {
-        nome_alt+="4"
-    } else {
-        nome_alt+=letra
-    }
-}
-console.log(nome_alt)
+const dara = new Date()
+console.log(dara)
